5aea8b4988a8292bd90ed7af9b0976c123e6f769
Bhumika G <Bhumika G@DESKTOP-3DVRLHB>
2023-07-04 14:00:09 +0530

    ok

---
+++ demomaven/src/test/java/com/demomaven/testsript/demoCustomer.java
@@ -0,0 +1,12 @@
+package com.demomaven.testsript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demoCustomer {
+	@Test
+	public void testDemoCustomer() {
+		Reporter.log("demoCustomer",true);
+	}
+
+}
