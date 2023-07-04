a7cf48c909090d5638f451c58912851b56ab45ee
BanuPriya93 <129736978+BanuPriya93@users.noreply.github.com>
2023-07-04 14:44:35 +0530

    Create demoDelete.java

---
+++ demomaven/demoDelete.java
@@ -0,0 +1,12 @@
+package com.demomaven.testsript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demoDelete {
+	@Test
+	public void testDemoDelete() {
+		Reporter.log("demoDelete",true);
+	}
+
+}
