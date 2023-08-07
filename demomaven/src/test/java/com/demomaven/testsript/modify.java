c0d9ec17d41e60999064327130f65c5d2947f797
BanuPriya93 <129736978+BanuPriya93@users.noreply.github.com>
2023-08-07 23:37:38 +0530

    Create modify.java

---
+++ demomaven/src/test/java/com/demomaven/testsript/modify.java
@@ -0,0 +1,12 @@
+package com.demomaven.testsript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class modifyCustomer {
+	@Test
+	public void testmodifyCustomer() {
+		Reporter.log("modifyCustomer",true);
+	}
+
+}
